42562297ed5f3bfcdaf417ceeb410bdd703aaa8e
Pedro-Haes <pedroz006@hotmail.com>
2024-07-31 21:28:27 -0300

    Implementação de todas as classes do projeto POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("SmartPhone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo Teste", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Modelo Teste 2", imei: "2222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
